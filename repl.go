54fb1a9faeeae596bb86494beba4a76845d116e3
ChaleArmando <chalejesus216@gmail.com>
2025-07-31 01:41:00 -0500

    REPL Loop integrated in the program, works as commands

--- repl.go
+++ repl.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
 	inputs := strings.Fields(strings.TrimSpace(strings.ToLower(text)))
 	return inputs
+}
+
+func replLoop() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleanInputSlice := cleanInput(input)
+		if len(cleanInputSlice) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", cleanInputSlice[0])
+	}
 }
